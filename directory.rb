28c2b9c5363a374bab625a472905a8c17c29c62b
Zsuzsanna Ver <56695526+MrsVer@users.noreply.github.com>
2021-05-18 10:57:54 +0100

    Create iteration

--- directory.rb
+++ directory.rb
@@ -11,19 +11,12 @@ students = [
   "Joffrey Baratheon",
   "Norman Bates"
 ]
-# and then print them
+
 puts "The students of Villains Academy"
 puts "-------------"
-puts students[0]
-puts students[1]
-puts students[2]
-puts students[3]
-puts students[4]
-puts students[5]
-puts students[6]
-puts students[7]
-puts students[8]
-puts students[9]
-puts students[10]
-# finally, we print the total
+
+students.each do |student|
+    puts student
+  end
+
 puts "Overall, we have #{student.count} great students"
