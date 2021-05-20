2297d6f406c87b4babb891cc221c5181e0fe1b82
Zsuzsanna Ver <56695526+MrsVer@users.noreply.github.com>
2021-05-20 10:17:44 +0100

    Save students to CSV file, Add more line to print_menu, Create .gitignore

--- directory.rb
+++ directory.rb
@@ -20,12 +20,6 @@ def interactive_menu
   end
 end
 
-def print_menu
-  puts "1. Input the students"
-  puts "2. Show the students"
-  puts "9. Exit" 
-end
-
 def show_students
   print_header
   print_student_list
@@ -38,6 +32,8 @@ def process(selection)
     input_students
   when "2"
     show_students
+when "3"
+    save_students
   when "9"
     exit 
   else
@@ -58,6 +54,23 @@ end
 
 def print_footer
   puts "Overall, we have #{@students.count} great students"
+end
+
+def save_students
+    file = File.open("students.csv", "w")
+    @students.each do |student|
+      student_data = [student[:name], student[:cohort]]
+      csv_line = student_data.join(",")
+      file.puts csv_line
+    end
+    file.close
+end
+
+def print_menu
+  puts "1. Input the students"
+  puts "2. Show the students"
+  puts "3. Save the list to students.csv"
+  puts "9. Exit" 
 end
 
 interactive_menu
